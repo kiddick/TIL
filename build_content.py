5c9203eb4a5d5ad31e85aa753ca080966c4ed154
kiddick <kiddick@users.noreply.github.com>
2016-09-07 12:58:18 +0300

    Fix code style.

--- build_content.py
+++ build_content.py
@@ -2,6 +2,11 @@ import datetime
 import os
 import re
 
+
+def todate(i):
+    i = i[:-3]
+    return datetime.datetime.strptime(i, '%m-%d-%Y')
+
 md_name = re.compile('\d{2}-\d{2}-\d{4}.md')
 md_list = []
 
@@ -10,14 +15,8 @@ for fname in os.listdir('.'):
     if md_name.match(fname):
         md_list.append(fname)
 
-
-def todate(i):
-    i = i[:-3]
-    return datetime.datetime.strptime(i, '%m-%d-%Y')
-
 md_list.sort(key=todate)
 
-print md_list
 with open('README.md', 'a') as readme:
     for fname in md_list:
         with open(fname, 'r') as _:
